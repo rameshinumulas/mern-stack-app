54c1818fab4f6d6ad811c853e924d4c4eda7ee3f
ramesh inumula <inumularamesh@gmail.com>
2024-11-28 21:39:09 +0530

    changes added

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,5 @@
-// export const BASE_URL =
-// process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : '';
-export const BASE_URL = '';
+export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : '';
+// export const BASE_URL = '';
 export const PRODUCTS_URL = '/api/v1/products';
 export const UPLOAD_URL = '/api/v1/upload';
 export const USERS_URL = '/api/v1/users';
