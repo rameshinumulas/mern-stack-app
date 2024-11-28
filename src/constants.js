8105340ccec90b83034e3311893d9100dcf88c75
ramesh inumula <inumularamesh@gmail.com>
2024-11-28 21:47:19 +0530

    origin changes

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : '';
+export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : 'https://mern-shop-abxs.onrender.com/';
 // export const BASE_URL = '';
 export const PRODUCTS_URL = '/api/v1/products';
 export const UPLOAD_URL = '/api/v1/upload';
